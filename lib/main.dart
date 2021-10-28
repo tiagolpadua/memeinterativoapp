996ff0ced10c6d8401753a8400088736482d7bcb
Tiago <tiagolpadua@gmail.com>
2021-10-27 23:58:13 -0300

    Tela Home

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
+      title: 'Meme Interativo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: const HomePage(),
     );
@@ -36,13 +27,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Olá Mundo!'
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 80),
+                child: Image(image: AssetImage('assets/img-tela-home.jpeg')),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
+                child: Text(
+                  'Descobrir como você starhoje nunca foi tão fácil!',
+                  style: TextStyle(fontSize: 24, color: Colors.white, fontFamily: 'Starjedi'),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
+                child: Text(
+                  'Saber como você está hoje é fundamental para organizar o trabalho com o time e garantir o aprendizado em equipe!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                    ),
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: const Text('COMEÇAR A USAR', style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -66,4 +92,3 @@ class ResultPage extends StatelessWidget {
     return Container();
   }
 }
-
